038d059e5e1decb4c3f798530893280a32bf2505
hpetter <henrik@hpetter.de>
2022-01-21 21:46:55 +0100

    added comments

--- helloworld.rs
+++ helloworld.rs
@@ -1,3 +1,13 @@
+// this is a single line comment
+
+/**This
+ * is
+ * a multi
+ * line
+ * comment 
+ */
+
 fn main(){
-    println!("Hello World")
+    println!("Hello World");
+    println!("I'm a Rustacean");
 }
