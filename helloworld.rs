200554e95691455a8a5af3f96cbf0260a89c7236
hpetter <henrik@hpetter.de>
2022-01-21 22:41:35 +0100

    basic string formatting

--- helloworld.rs
+++ helloworld.rs
@@ -1,13 +1,17 @@
 // this is a single line comment
 
-/**This
- * is
- * a multi
- * line
- * comment 
+/* 
+ This is a multi line comment 
+ can also be used for inline comments
  */
 
+ /// doc comment which is parsed into html library
+ /// are being compiled into documentation running ```rustdoc```
+ /// markdown friendly
+
 fn main(){
-    println!("Hello World");
-    println!("I'm a Rustacean");
+    // {} is being replaced by arguments
+    println!("{} World", "Hello"); //output "Hello World"
+    // {} can be filled with numbers or variable names
+    println!("{subject} {verb} {object}",object="a Rustacean", subject="I", verb="am");
 }
