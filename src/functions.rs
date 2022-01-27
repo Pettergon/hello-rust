10764f7cbbf1d1a332ae5df55a6c0183c7e57429
hpetter <henrik@hpetter.de>
2022-01-27 20:14:27 +0100

    functions and references

--- src/functions.rs
+++ src/functions.rs
@@ -5,9 +5,10 @@ pub fn run(){
     let get_sum = add(5, 5);
     println!("Sum: {}", get_sum);
 
+    // closure
     let n3: i32 = 10;
     let add_nums = |n1: i32, n2: i32| n1 + n2 + n3;
-    println!("C Sum: {}", add_nums(3, 3));
+    println!("Closure Sum: {}", add_nums(3, 3));
 
 }
 
@@ -15,6 +16,8 @@ fn greeting(greet: &str, name: &str) {
     println!("{} {}", greet, name);
 }
 
+// arrow to return something
 fn add(n1: i32, n2: i32) -> i32 {
+    // no semicolon to tell it to return the line
     n1 + n2
 }
